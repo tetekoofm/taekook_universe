2d8c34fb0a789be8279cb3b17a17ee800a01a33a
tetekoofm <tetekoofm@gmail.com>
2024-12-17 21:07:06 -0600

    Bind Flask app to the correct port

--- app.py
+++ app.py
@@ -19,4 +19,5 @@ def projects():
 
 # Run the app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get('PORT', 10000))  # Use the environment variable for Render or default to 10000
+    app.run(host='0.0.0.0', port=port)
