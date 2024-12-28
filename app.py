f5522488c0e9addcd2534a08fe5bc557b5cf604b
tetekoofm <tetekoofm@gmail.com>
2024-12-28 15:43:10 -0600

    Removed ssl part

--- app.py
+++ app.py
@@ -222,4 +222,5 @@ def checkout():
 
 # Start the Flask app
 if __name__ == "__main__":
-    app.run(debug=True, ssl_context=('cert.pem', 'cert.key'), host="0.0.0.0", port=10000)
+    app.run(debug=True, host="0.0.0.0", port=10000)
+    # app.run(debug=True, ssl_context=('cert.pem', 'cert.key'), host="0.0.0.0", port=10000)
