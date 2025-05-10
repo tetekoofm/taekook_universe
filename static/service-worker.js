3023927d640eb962d4cc62d26dcd9f5cf7ea5709
tetekoofm <tetekoofm@gmail.com>
2025-05-09 21:56:38 -0500

    fixed icons error

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,8 +1,7 @@
 const CACHE_NAME = 'tku-cache-v1';
 const urlsToCache = [
   '/',
-  '/static/icon_small.png',
-  '/static/icon_big.png',
+  '/static/favicon.png',
   '/static/manifest.json',
   // Add more paths if needed like CSS, JS, logo, etc.
 ];
@@ -12,7 +11,7 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('v1').then((cache) => {
       return cache.addAll([
-        '/static/icons/icon_small.png',
+        '/static/favicon.png',
         '/static/style.css'
         // Add other critical assets
       ]);
@@ -20,12 +19,39 @@ self.addEventListener('install', (e) => {
   );
 });
 
-
 // Fetch event
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = ['tku-cache-v1']; // Update this version if the cache changes
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('push', (event) => {
+  const options = {
+    body: event.data.text(),
+    icon: '/static/favicon.png',
+    badge: '/static/favicon.png'
+  };
+
+  event.waitUntil(
+    self.registration.showNotification('New Notification!', options)
   );
 });
